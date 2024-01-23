67f7924e2bdbb0e39c976307bb41452716be54ea
andres <arozados@educa.madrid.org>
2024-01-23 14:09:03 +0100

    hecho el 2

--- tema8/localStorage/ejercicio2/index.js
+++ tema8/localStorage/ejercicio2/index.js
@@ -1,7 +1,14 @@
 
-function anadir() {
 
-        localStorage.setItem("contador",toString(contador++));
-        document.getElementById('contador').innerHTML=localStorage.getItem('contador')
+let contador=parseInt(document.getElementById('contador').innerHTML);
+function anadir() {
+        console.log(contador)
+        console.log(typeof(contador));
+        localStorage.setItem("contador",contador++);
+
+        console.log(typeOf(localStorage.getItem('contador')))
+        // document.getElementById('contador').innerHTML=toString(localStorage.getItem('contador'));
+
+
 
 }
