a694df16f8c83fc86ffa980db546d089c14d15ce
andres <arozados@educa.madrid.org>
2024-01-24 13:37:55 +0100

    tema 8 ejercicio 1

--- tema8/ejercicio1/ejercicio1.js
+++ tema8/ejercicio1/ejercicio1.js
@@ -2,8 +2,9 @@ function estaBienNombre(nombre) {
     regx=/^(([A-z]){1,})+\s+(([A-z]){1,})+\s+(([A-z]){1,})$/;
     return regx.test(nombre);
 }
-document.getElementById("button").addEventListener('click', function (event){
-    
+
+document.getElementById('formularioPedido').addEventListener('submit', function (event){
+    event.preventDefault();
 
     document.getElementById('nombre').value
     if(estaBienNombre(document.getElementById('nombre').value)){
@@ -14,7 +15,7 @@ document.getElementById("button").addEventListener('click', function (event){
         return false
     }
     
-    if(document.querySelector('input[name="numeroPie"]:checked')){
+    if(document.querySelector('input[name="talla"]:checked')){
       
         
     }else{
@@ -22,18 +23,35 @@ document.getElementById("button").addEventListener('click', function (event){
         return false
     }
    
-    if(document.querySelectorAll('input:checked').length>0){
-       
+    if(document.querySelectorAll('input[type="checkbox"]:checked').length>0){
 
     }else{
         window.alert("Tienes que seleccionar por lo menos un color")
         return false
     }
+    let resultado="Los valores enviados son: ";
+
+    //Introduciendo el nombre en el resultado
+    let nombreElegido=document.getElementById('nombre').value;
+    resultado=resultado+" "+nombreElegido;
+
+    //introduciendo el resto de valores seleccionados en el resultado
+    let cosasMarcadas=document.querySelectorAll('input:checked')
+
+    cosasMarcadas.forEach(elementos => {
+        resultado=resultado+" "+elementos.value;
+    });
+    
+    
+    //Borrando los lvalores del formulario
+
     document.getElementById('nombre').value="";
-    document.querySelector('input[name="numeroPie"]:checked').checked=true;
     document.querySelectorAll('input:checked').forEach(element => {
         element.checked=false;
     });
+
+    //Realizando el alert
+    window.alert(resultado)
 });
 
 
