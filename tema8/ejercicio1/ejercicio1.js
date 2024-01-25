e31925c642e167d617110a3e29be9da05fd77b80
andres <arozados@educa.madrid.org>
2024-01-25 07:54:45 +0100

    hecho 1 y el 2 tema 8

--- tema8/ejercicio1/ejercicio1.js
+++ tema8/ejercicio1/ejercicio1.js
@@ -1,39 +1,47 @@
+//Funcion que realiza el regx para el nombre
 function estaBienNombre(nombre) {
     regx=/^(([A-z]){1,})+\s+(([A-z]){1,})+\s+(([A-z]){1,})$/;
     return regx.test(nombre);
 }
-document.getElementById("button").addEventListener('click', function (event){
-    
+
+document.getElementById("formularioPedido").addEventListener('submit', function (event){
+    event.preventDefault()
 
     document.getElementById('nombre').value
-    if(estaBienNombre(document.getElementById('nombre').value)){
-        
-        
-    }else{
+    if(!estaBienNombre(document.getElementById('nombre').value)){
         window.alert("No se ha introducido correctamente el nombre de la persona")
         return false
-    }
-    
-    if(document.querySelector('input[name="numeroPie"]:checked')){
-      
         
-    }else{
+        
+    }
+    if(!document.querySelector('input[name="talla"]:checked')){
         window.alert("No se ha introducido correctamente el numero de pie")
         return false
+      
+        
     }
    
-    if(document.querySelectorAll('input:checked').length>0){
-       
-
-    }else{
+    if(!document.querySelectorAll('input[name="color"]:checked').length>0){
         window.alert("Tienes que seleccionar por lo menos un color")
         return false
+
     }
+    
+    let resultado="Se ha mandado los siguientes valores:";
+    resultado=resultado+" "+document.getElementById('nombre').value;
+
+    //Eliminando el nombre en el formulairo
     document.getElementById('nombre').value="";
-    document.querySelector('input[name="numeroPie"]:checked').checked=true;
+
+    //borrarndo todos los input checkeados y pasado al resultado los valores
     document.querySelectorAll('input:checked').forEach(element => {
+        resultado=resultado+" "+element.value
         element.checked=false;
     });
+
+    //muestra el window alert con la frase
+    window.alert(resultado)
+
 });
 
 
--- tema8/ejercicio2/ejercicio2.js
+++ tema8/ejercicio2/ejercicio2.js
@@ -1,24 +1,81 @@
 const destinos={
     "Destinos":[
-        {"valencia":["Elche","Orihuela"],
-        "Alicante":["Valencia Capital","Torrent"]
-        }]
+        {"Alicante":["Alicante Capital","Elche","Orihuela"]},
+        {"Valencia":["Mislata","Valencia Capital","Torrent"]}
+        ]
 
 }
 
 
 const primerDestino= document.getElementById('primerdestino');
+
+//Cambiar el destino dinamicamente
 primerDestino.addEventListener("change",(e)=>{
-    const etiqueta=e.currentTarget;
+    const etiqueta=e.target;
     const elemento=document.querySelector('option[value="'+etiqueta.value+'"]')
-    const origenNombre=elemento.innerHTML;
-    detinos["Destinos"].forEach(elemento => {
-        elemento[origenNombre];
+    const origenNombreSeleccionado=elemento.innerHTML;
+
+    destinos["Destinos"].forEach(destino => {
+        const destinoEtiqueta=document.getElementById('segundodestino');
+        destinoEtiqueta.innerHTML="" //Eliminando cualquier contendio que esta dentro de los select 
+        console.log("suelta:"+destino[origenNombreSeleccionado])
+        console.log(origenNombreSeleccionado);
+        //Introduciendo los destinos dentro de select
+        // destino[origenNombreSeleccionado].forEach(destino =>{
+        //     let opciones=document.createElement('option');
+        //     opciones.innerHTML=destino;
+        //     destinoEtiqueta.appendChild(opciones);
+            
+        // });
     });
 
 
 })
 document.cookie = "nombre=oeschger";
 document.cookie = "comida_preferida=tripa";
+
+
+//Comprueba si el usuario tiene un nombre y por lo menos el primer apellido introducido
+function estaBienNombre(nombre) {
+    regx=/^(([A-z]){1,})+\s+(([A-z]){1,})+(\s+([A-z]){0,}){0,}$/;
+    return regx.test(nombre);
+}
+
+document.querySelector('form').addEventListener('submit',(e)=>{
+    event.preventDefault()
+
+    //validando el nombre
+    if(!estaBienNombre(document.getElementById('username').value)){
+        window.alert("no ha introducido correctamente el usuario usuario tiene que tener un nombre y por lo menos el primer apellido introducido");
+        return false
+    }
+    
+    let fecha =new Date();
+    
+    //Comprobando el año introducido coresponde con el año actual
+    if(document.getElementById('travelDate').value.split('-')[0] !=fecha.getFullYear()){
+        window.alert("introduzca una fecha que contenga el año actual")
+        return false
+    }else{
+    }
+    
+    //Comprobando que hay un radiobutton pulsado
+    if(document.querySelector('input:checked') === null){ 
+        window.alert("Pulse algun radio boton")
+        return false
+    }
+
+    //Comprobando que si o si pongan la frase Madrid , estación sur fijo en origen
+    if(document.getElementById('origen').value !== "Madrid , estación sur fijo"){
+        window.alert("Origen es incorrecto debe poner exactamente:Madrid , estación sur fijo")
+    }
+
+    //Se introduce en el if cuando no se elige un destino
+    if(document.getElementById('segundodestino').value === "Select no seleccionado"){
+        window.alert("seleecione el destino")
+    }
+
+
+})
 
 
