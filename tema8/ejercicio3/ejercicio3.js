93b43b95f4b1af0f0093a258b8d2173055d046e6
andres <arozados@educa.madrid.org>
2024-01-27 22:51:53 +0100

    corregidas algunas cosas

--- tema8/ejercicio3/ejercicio3.js
+++ tema8/ejercicio3/ejercicio3.js
@@ -1,19 +1,59 @@
-function guardarCookie(){
-    if(document.getElementById('cookieDays').value !=null){
-
-
-        const nombreCookie=document.getElementById('cookieName').value
-        const valorCookie=document.getElementById('cookieValue').value
-        const duracionCookie=parseInt(document.getElementById('cookieDays').value)
-        
-        let fechaActual=new Date()
-        console.log(fechaActual)
-        fechaActual.setDate(fechaActual.getDate() + duracionCookie);
-        
-        document.cookie =  nombreCookie+'='+valorCookie+'; expires='+fechaActual.toUTCString()
+//compruba si esta vacio el elemnto 
+function campoCorrecto(elmentoComprobar){
+    let resultado;
+    if(elmentoComprobar.value !=""){
+        resultado= true
+    }else{
+ 
+        let elemento=document.createElement('p');
+        elemento.innerHTML="Tiene que rellenar el campo";
+        elemento.style.color="red";
+        elemento.setAttribute("class","error");
+        elmentoComprobar.after(elemento)
+        resultado = false
+    
+        resultado=false;
     }
+    return resultado;
 }
 
+//guarda las cookies
+function guardarCookie(){
+        let nombreEtiqueta=document.getElementById('cookieName')
+        let valorCookieEtiqueta=document.getElementById('cookieValue')
+        let duracionCookieEtiqueta=document.getElementById('cookieDays')
+        
+
+        if(campoCorrecto(nombreEtiqueta) && campoCorrecto(valorCookieEtiqueta) && campoCorrecto(duracionCookieEtiqueta)){
+
+            const nombreCookie=nombreEtiqueta.value
+            const valorCookie=valorCookieEtiqueta.value
+            const duracionCookie=duracionCookieEtiqueta.value
+            
+            let fechaActual=new Date()
+            console.log(fechaActual)
+            fechaActual.setDate(fechaActual.getDate() + duracionCookie);
+            
+            document.cookie =  nombreCookie+'='+valorCookie+'; expires='+fechaActual.toUTCString()
+            
+            //Eliminando los valores de cookies y los errores
+            const elementosErrores=document.querySelectorAll('p[class="error"]')
+            const elementosInputs=document.getElementsByTagName("input")
+            console.log(elementosErrores)
+            for (let index = 0; index < elementosErrores.length; index++) {
+                elementosErrores[index].remove()
+                
+            }
+            for (let index = 0; index < elementosInputs.length; index++) {
+                elementosInputs[index].value=""
+                
+            }
+            console.log("hecho")
+        }
+   
+}
+
+//Se encarga de mostrar las cookies por pantalla (no borra las cookies mostradas anteriormente)
 function mostrarCookies() {
     const cookiesGuaredas=document.cookie.split('; ')
 
--- tema8/localStorage/ejercicio5/index.js
+++ tema8/localStorage/ejercicio5/index.js
@@ -1,18 +1,28 @@
 
+//Generar en localStorage la variable contador si no existe
+if(!localStorage.getItem("contador")){//si no existe se genera la variable
+        localStorage.setItem("contador",0);
 
-let contador=parseInt(document.getElementById('contador').innerHTML);
+}
 
+//añadir un numero al contador
 function anadir() {
-        localStorage.setItem("contador",contador++);
+        let contador=localStorage.getItem("contador")
+        contador++
+        localStorage.setItem("contador",contador);
         document.getElementById('contador').innerHTML=localStorage.getItem('contador');
 
 
 
 }
-document.getElementById('botonEliminar').addEventListener("click",function () {
-        let numero=localStorage.getItem('contador')--
-        if(numero>0){
-                document.getElementById('contador').innerHTML=localStorage.getItem('contador');
+//restar un contador y deja de restar cuando llega a 0
+document.getElementById('botonEliminar').addEventListener("click",function (e) {
+        let numero=localStorage.getItem("contador")
+        numero--
+        if(numero>=0){
+                document.getElementById('contador').innerHTML=numero;
+                localStorage.setItem("contador",numero);
         }
+
         
 });
