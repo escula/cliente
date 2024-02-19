0d67a2b078c64ae8f2289e65bd725330aa306c55
andres <arozados@educa.madrid.org>
2024-02-19 17:15:59 +0100

    realizado ejercicio1 del tema 9 de cliente

---
+++ tema9/ejercicio2 envio a servidor por ajax/servidor2.php
@@ -0,0 +1,19 @@
+<?php
+// Verificar si se recibieron datos en formato JSON en la solicitud POST
+$data = json_decode(file_get_contents("php://input"));
+
+// Verificar si se recibió el nombre en los datos enviados
+if(isset($data->nombre)) {
+    // Obtener el nombre recibido
+    $nombre = $data->nombre;
+
+    // Construir el mensaje de saludo con el nombre recibido
+    $mensaje = "¡Hola, " . $nombre . "!";
+
+    // Devolver el mensaje de saludo al cliente (página web)
+    echo $mensaje;
+} else {
+    // Si no se recibió el nombre, devolver un mensaje de error al cliente
+    echo "Error: Nombre no recibido.";
+}
+?>
